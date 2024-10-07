8283560752a3f667e35c0b44927be7bfe9601457
Nicholas Monforte <nmonforte@CI0600000003193.rim.net>
2024-10-07 10:55:34 -0400

    adding highlight when yank

--- nvim/init.lua
+++ nvim/init.lua
@@ -39,3 +39,15 @@ telescope.setup({
     }
   }
 })
+
+local autocmd = vim.api.nvim_create_autocmd
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
