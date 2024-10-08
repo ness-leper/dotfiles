0a1dd181382618ec294c4da8dbff781ee0c696db
Nicholas Monforte <nmonforte@CI0600000003193.rim.net>
2024-10-08 14:33:59 -0400

    switching key bind

--- nvim/lua/plugins/terminal.lua
+++ nvim/lua/plugins/terminal.lua
@@ -9,7 +9,7 @@ return {
       },
     })
 
-    vim.keymap.set("n", "<leader>i", '<CMD>lua require("FTerm").toggle()<CR>')
-    vim.keymap.set("t", "<leader>i", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+    vim.keymap.set("n", "<C-i>", '<CMD>lua require("FTerm").toggle()<CR>')
+    vim.keymap.set("t", "<C-i>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
   end,
 }
