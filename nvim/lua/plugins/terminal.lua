2b99274a181a01008dcb279074d1490d77121a4a
Nicholas Monforte <nmonforte@CI0600000003193.rim.net>
2024-10-08 14:13:25 -0400

    switching the terminal activation key

--- nvim/lua/plugins/terminal.lua
+++ nvim/lua/plugins/terminal.lua
@@ -9,7 +9,7 @@ return {
       },
     })
 
-    vim.keymap.set("n", "<A-i>", '<CMD>lua require("FTerm").toggle()<CR>')
-    vim.keymap.set("t", "<A-i>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+    vim.keymap.set("n", "<leader>i", '<CMD>lua require("FTerm").toggle()<CR>')
+    vim.keymap.set("t", "<leader>i", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
   end,
 }
