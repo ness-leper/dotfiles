7e49708160e45605ea6871dcb50c7dc0942ce0ce
Nicholas Monforte <nmonforte@CI0600000003193.rim.net>
2024-10-08 15:31:26 -0400

    switching back

--- nvim/lua/plugins/terminal.lua
+++ nvim/lua/plugins/terminal.lua
@@ -9,7 +9,7 @@ return {
       },
     })
 
-    vim.keymap.set("n", "<C-i>", '<CMD>lua require("FTerm").toggle()<CR>')
-    vim.keymap.set("t", "<C-i>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+    vim.keymap.set("n", "<A-i>", '<CMD>lua require("FTerm").toggle()<CR>')
+    vim.keymap.set("t", "<A-i>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
   end,
 }
