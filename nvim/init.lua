66488825b51efbf6a18dc61886ec6903404cf98c
Nicholas Monforte <nmonforte@CI0600000003193.rim.net>
2024-12-04 13:59:14 -0500

    adding leader w to save

--- nvim/init.lua
+++ nvim/init.lua
@@ -24,6 +24,7 @@ vim.keymap.set("v", ">", ">gv", {})
 vim.keymap.set("n", "<C-h>", ":wincmd h <CR>", {})
 vim.keymap.set("n", "<C-j>", ":wincmd j <CR>", {})
 vim.keymap.set("n", "<C-l>", ":wincmd l <CR>", {})
+vim.keymap.set("n", "<leader>w", ":w <CR>", {})
 
 vim.keymap.set("n", "<leader>qq", ":bd <CR>", {})
 
