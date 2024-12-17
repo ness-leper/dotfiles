165d802d433d4077700bbb08fcc48f94012cf957
Nicholas Monforte <nmonforte@CI0600000003193.rim.net>
2024-12-17 14:28:25 -0500

    adding screenshot copy to clipboard and notification

--- sway/screenshot.sh
+++ sway/screenshot.sh
@@ -2,3 +2,5 @@
 FILENAME="screenshot-`date +%F-%T`"
 grim -g "$(slurp)" ~/Pictures/Screenshots/$FILENAME.png
 echo $FILENAME
+wl-copy < ~/Pictures/Screenshots/$FILENAME.png 
+notify-send $FILENAME "Copied to clipboard!"
