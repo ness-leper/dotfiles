a9f1e200ca6a80ec4b5502ad6f921011776344b3
Nicholas Monforte <nmonforte@CI0600000003193.rim.net>
2024-12-18 14:09:42 -0500

    updating blink.cmp

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -1,7 +1,10 @@
 return {
   {
     'saghen/blink.cmp',
-    dependencies = 'rafamadriz/friendly-snippets',
+    dependencies = {
+      'rafamadriz/friendly-snippets',
+      "folke/lazydev.nvim",
+    },
 
     version = 'v0.*',
     opts = {
@@ -13,8 +16,42 @@ return {
       },
 
       sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer' },
+        completion = {
+          enabled_providers = { "lsp", "path", "snippets", "buffer", "lazydev" },
+        },
+        providers = {
+          lsp = { fallback_for = { "lazydev" } },
+          lazydev = { name = "LazyDev", module = "lazydev.integrations.blink" },
+        }
+        -- default = { 'lsp', 'path', 'snippets', 'buffer' },
       },
+      completion = {
+        accept = { auto_brackets = { enabled = true } },
+        trigger = {
+          show_on_insert_on_trigger_character = false,
+        },
+        menu = {
+          border = vim.g.border_style,
+          scrolloff = 1,
+          scrollbar = false,
+          columns = { { 'kind_icon' }, { 'label', 'label_description', gap = 0 } },
+
+          draw = {
+            padding = 0,
+            gap = 1,
+            treesitter = true,
+          }
+        },
+      },
+
+      documentation = {
+        auto_show_delay_ms = 0,
+        auto_show = true,
+        window = {
+          border = vim.g.border_style,
+        },
+      },
+
 
       signature = { enabled = true }
     },
