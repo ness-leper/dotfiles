996adeb9e360ee9039e92392961cd68b6ec14815
nick <nmonforte@gmail.com>
2025-04-18 23:42:26 -0400

    adding some new configs

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -1,8 +1,8 @@
 return {
-  "dracula/vim",
-  name="dracula",
+  "ellisonleao/gruvbox",
+  name="gruvbox",
   priority=1000,
   config = function()
-    vim.cmd.colorscheme "dracula"
+    vim.cmd.colorscheme "gruvbox"
   end
 }
