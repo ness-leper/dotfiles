4f547122d7eaa601d1f4fe39f9a911af0fcf84d3
nick <nmonforte@gmail.com>
2025-05-06 21:29:23 -0400

    showing hidden files

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,14 +1,16 @@
-return {  
-  'stevearc/oil.nvim',  
-  config = function()   
-    require('oil').setup({    
-      keymaps = {     
-        ["<Esc>"] = "actions.close"    
-      }   
-    })  
-  end,  
-  keys = {   
-    { '=', '<cmd>Oil<cr>', mode = 'n', desc = "Open Filesystem" },   
-    { '-', '<cmd>Oil<cr>', mode = 'n', desc = "Open Floating Filesystem" },  
-  } 
-} 
+return {
+  'stevearc/oil.nvim',
+  config = function()
+    require('oil').setup({
+      keymaps = {
+        ["<Esc>"] = "actions.close"
+      },
+      view_options = {
+        show_hidden = true
+      },
+    })
+  end,
+  keys = {
+    { '-', '<cmd>Oil<cr>', mode = 'n', desc = "Open Floating Filesystem" },
+  }
+}
