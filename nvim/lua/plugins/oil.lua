9d781504015f948e098eeed315f0edbf18498426
nick <nmonforte@gmail.com>
2025-05-06 21:27:44 -0400

    formatting

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,1 +1,14 @@
-return {  'stevearc/oil.nvim',  config = function()   require('oil').setup({    keymaps = {     ["<Esc>"] = "actions.close"    }   })  end,  keys = {   { '=', '<cmd>Oil<cr>', mode = 'n', desc = "Open Filesystem" },   { '-', '<cmd>Oil<cr>', mode = 'n', desc = "Open Floating Filesystem" },  } } 
+return {  
+  'stevearc/oil.nvim',  
+  config = function()   
+    require('oil').setup({    
+      keymaps = {     
+        ["<Esc>"] = "actions.close"    
+      }   
+    })  
+  end,  
+  keys = {   
+    { '=', '<cmd>Oil<cr>', mode = 'n', desc = "Open Filesystem" },   
+    { '-', '<cmd>Oil<cr>', mode = 'n', desc = "Open Floating Filesystem" },  
+  } 
+} 
